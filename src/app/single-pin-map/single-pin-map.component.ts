bf4bef6b81a66ef87425f8cdd8f1c84c1fb41990
Huda <hudaelzubair@hotmail.com>
2022-08-16 21:45:22 +0300

    single map routing integrating +final push

--- src/app/single-pin-map/single-pin-map.component.ts
+++ src/app/single-pin-map/single-pin-map.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import * as mapboxgl from 'mapbox-gl';
+import { Router, ActivatedRoute, ParamMap,Navigation } from '@angular/router';
 
 @Component({
   selector: 'single-pin-map',
@@ -8,22 +9,39 @@ import * as mapboxgl from 'mapbox-gl';
   styleUrls: ['./single-pin-map.component.css']
 })
 export class SinglePinMapComponent implements OnInit {
+  
   map: mapboxgl.Map;
-  constructor() { }
+  longitude:number ;
+  latitude:number ;
+  pinName: string;
+  constructor(private route: ActivatedRoute) {
+   this.longitude= this.route.snapshot.params['longitude'];
+   this.latitude=this.route.snapshot.params['latitude'];
+   this.pinName=this.route.snapshot.params['name'];
+    // this.route.snapshot.paramMap.get(longitude);
+   // this.route.snapshot.paramMap.get(latitude);
+console.log('console.log',this.longitude,this.latitude);
+   }
+   
 
   ngOnInit() {
+    
     (mapboxgl as any).accessToken = environment.mapboxkey;
     this.map = new mapboxgl.Map({
     container: 'mapbox-pin-map', // container ID
     style: 'https://api.maptiler.com/maps/eef16200-c4cc-4285-9370-c71ca24bb42d/style.json?key=CH1cYDfxBV9ZBu1lHGqh', // style URL
-    center: [-95.601961, 29.68039], // starting position
-    zoom: 10.38 // starting zoom
+    center: [this.longitude,this.latitude], // starting position
+    zoom: 16// starting zoom
     
     });
+   // this.map.resize();
     this.map.addControl(new mapboxgl.NavigationControl());
    
-    this.createPin(-95.563694,29.687847);  
+    this.createPin(this.longitude,this.latitude);  
+
   }
+ 
+
 
   createPin(lng:number,lat:number){
     //const lngLat = popup.getLngLat();
@@ -31,7 +49,7 @@ export class SinglePinMapComponent implements OnInit {
         color: "#000000",
         draggable: true
       }).setLngLat([lng,lat])
-     // .setPopup(new mapboxgl.Popup().setText('pins'))
+    .setPopup(new mapboxgl.Popup().setText(this.pinName))
         .addTo(this.map);
   }
 
